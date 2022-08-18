246dac62edd88eddf93f6008242f9cc1926abd07
Garrett Hays <GarrettlHays@gmail.com>
2022-08-18 13:07:21 -0700

    adds teams controller, model and view

---
+++ AdminPortal/Controllers/TeamsController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AdminPortal.Models;
+
+namespace AdminPortal.Controllers
+{
+  public class TeamsController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allTeams = Team.GetTeams();
+      return View(allTeams);
+    }
+
+    [HttpPost]
+    public IActionResult Index(Team team)
+    {
+      Team.Post(team);
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Details(int id)
+    {
+      var team = Team.GetDetails(id);
+      return View(team);
+    }
+
+    public IActionResult Edit(int id)
+    {
+      var team = Team.GetDetails(id);
+      return View(team);
+    }
+
+    [HttpPost]
+    public IActionResult Details(int id, Team team)
+    {
+      team.TeamId = id;
+      Team.Put(team);
+      return RedirectToAction("Details", id);
+    }
+
+    public IActionResult Delete(int id)
+    {
+      Team.Delete(id);
+      return RedirectToAction("Index");
+    }
+  }
+}
--- AdminPortal/Models/ApiHelper.cs
+++ AdminPortal/Models/ApiHelper.cs
@@ -52,6 +52,38 @@ namespace AdminPortal.Models
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
+
+    public static async Task<string> GetTeam(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"animals/{id}", Method.GET);
+      request.AddHeader("Authorization", "Bearer " + TokenC.Token);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+    public static async Task PostTeam(string newAnimal)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"animals", Method.POST);
+      request.AddHeader("Authorization", "Bearer " + TokenC.Token);
+      request.AddJsonBody(newAnimal);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+    public static async Task PutTeam(int id, string newAnimal)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"animals/{id}", Method.PUT);
+      request.AddHeader("Authorization", "Bearer " + TokenC.Token);
+      request.AddJsonBody(newAnimal);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+    public static async Task DeleteTeam(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"animals/{id}", Method.DELETE);
+      request.AddHeader("Authorization", "Bearer " + TokenC.Token);
+      var response = await client.ExecuteTaskAsync(request);
+    }
     public static async Task<string> LogIn(LoginViewModel user)
     {
       RestClient client = new RestClient("http://localhost:5000/api");
